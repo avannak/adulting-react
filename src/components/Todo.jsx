6a312673d0b5325a0cf20413da3a199fbc4d4209
avannaki@mail.sfsu.edu <arthurvat7@gmail.com>
2022-04-04 19:53:55 -0700

    add unsaveable color changer

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,8 +8,7 @@ import { Draggable } from 'react-beautiful-dnd';
 export let removeItem;
 export let completeTxt;
 export let updateItem;
-
-
+// let currentClr = 'red';
 // function to help us with reordering the result
 export const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
@@ -29,6 +28,7 @@ export const filterText = (text) => {
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
 
     let [currentColor, setCurrentColor] = useState('white');
+    // const [bg, changeBGColor] = React.useState(1);
 
     const [edit, setEdit] = useState({
         id: null,
@@ -37,7 +37,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     // const componentRef = React.useRef();
     const rowRef = React.useRef(null);
 
-    const getItemStyle = (isDragging, draggableStyle, id) => ({
+    const getItemStyle = (isDragging, draggableStyle, index) => ({
         // basic styles to make the items look a bit nicer
         userSelect: "none",
         width: "100%",
@@ -52,20 +52,25 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
         ...draggableStyle
     });
 
-    const changeRowColor = (color) => {
-        console.log(rowRef);
-        // if (color === 'white') {
-        //     setCurrentColor('white');
-        // }
-        // console.log(id);
-        // if (currentColor === 'white') {
-        //     setCurrentColor('red');
-        // }
-        // if (currentColor === 'red') {
-        //     setCurrentColor('white');
-        // }
-    }
+    const changeColor = (index, provided) => {
+        let element = document.getElementById(index);
+        // console.log(currentColor);
+        // console.log(element.backgroundColor);
+        if (element.style.backgroundColor === 'white') {
+            // setCurrentColor('blue');
+            return element.style.backgroundColor = "lightblue";
+        }
+        if (element.style.backgroundColor === 'lightblue') {
+            return element.style.backgroundColor = "lightcoral";
+        }
+        if (element.style.backgroundColor === 'lightcoral') {
+            return element.style.backgroundColor = "rgb(142, 252, 142)";
+        }
+        if (element.style.backgroundColor === 'rgb(142, 252, 142)') {
+            return element.style.backgroundColor = "white";
+        }
 
+    }
 
     const submitUpdate = value => {
         let prevValue = updateItem;
@@ -78,27 +83,29 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     }
 
 
-
     if (edit.id) {
 
         return <TodoForm edit={edit} onSubmit={submitUpdate} />;
     }
 
+
     return (
         todos.map((todo, index) =>
 
-            <div key={todo.id} className={todo.isComplete ? 'todo-row complete' : 'todo-row'} >
-                <Draggable key={todo.id} draggableId={todo.id} index={index}>
+            <div ref={rowRef} key={index} className={todo.isComplete ? 'todo-row complete' : 'todo-row'} >
+                <Draggable key={todo.id} draggableId={todo.id} index={index} >
                     {(provided, snapshot) => (
-                        <div className="list-item" ref={provided.innerRef}
+                        <div id={index} className="list-item" ref={provided.innerRef}
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                             style={getItemStyle(
                                 snapshot.isDragging,
                                 provided.draggableProps.style,
+                                index
+                                // currentClr
                             )}>
 
-                            <div ref={rowRef} className="todo-text" key={todo.id} onClick={() => completeTodo(todo.id, completeTxt = filterText(todo.text))}>
+                            <div className="todo-text" key={todo.id} onClick={() => completeTodo(todo.id, completeTxt = filterText(todo.text))}>
                                 {todo.text}
                             </div>
                             <div className="icons">
@@ -115,10 +122,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
                                     }} />
                                 </div>
                                 <div className="change-color-icon">
-                                    <HiOutlineColorSwatch size={28} onClick={() => {
-                                        console.log(index);
-                                        console.log(todo.id)
-                                    }} />
+                                    <HiOutlineColorSwatch size={28} onClick={() => { changeColor(index, provided) }} />
                                 </div>
                             </div>
                         </div>
