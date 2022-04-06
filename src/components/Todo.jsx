e83c620571cdbaac131e7ce3da086ff4f12823ad
avannaki@mail.sfsu.edu <arthurvat7@gmail.com>
2022-04-06 00:26:58 -0700

    add non-saveable color change

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -18,6 +18,7 @@ export const reorder = (list, startIndex, endIndex) => {
     return result;
 };
 
+export const ThemeContext = React.createContext()
 
 
 export const filterText = (text) => {
@@ -26,8 +27,9 @@ export const filterText = (text) => {
 }
 
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+    let [trigger, setTrigger] = useState(false);
+    // let [currentColor, setCurrentColor] = useState(null);
 
-    let [currentColor, setCurrentColor] = useState('white');
     // const [bg, changeBGColor] = React.useState(1);
 
     const [edit, setEdit] = useState({
@@ -37,42 +39,68 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     // const componentRef = React.useRef();
     const rowRef = React.useRef(null);
 
-    const getItemStyle = (isDragging, draggableStyle, index) => ({
+    const getItemStyle = (isDragging, draggableStyle, rowId) => ({
         // basic styles to make the items look a bit nicer
         userSelect: "none",
         width: "100%",
         padding: "5px",
 
-        //if id matches ref id, change color of element that matches the id
+        //if not dragging, set color to what document element by id is currently
 
         // change background colour if dragging
-        background: isDragging ? "lightgreen" : currentColor,
-        boxShadow: isDragging ? "0 0 .4 rem #666" : "none",
-        // styles we need to apply on draggables
+        backgroundColor: trigger === false ? setColorTo(rowId, trigger) : setColorTo(rowId, trigger),
+        // background: isDragging ? "lightgreen" : setColorTo(rowId, trigger),
+        boxShadow: isDragging ? "2px 2px 10px black" : "none",
+        // styles we need to apply on draggables 
         ...draggableStyle
     });
+    const setColorTo = (rowId, triggerFinger) => {
+        if (triggerFinger === false) {
+            setTrigger(true);
+            return 'white';
+        } else if (triggerFinger === true) {
 
-    const changeColor = (index, provided) => {
-        let element = document.getElementById(index);
+            return document.getElementById(rowId).style.backgroundColor;
+        } else {
+            return 'white';
+        }
+    }
+
+    const changeColor = (rowId, provided) => {
+
+        let element = document.getElementById(rowId);
+        // console.log(provided);
         // console.log(currentColor);
         // console.log(element.backgroundColor);
         if (element.style.backgroundColor === 'white') {
-            // setCurrentColor('blue');
-            return element.style.backgroundColor = "lightblue";
-        }
-        if (element.style.backgroundColor === 'lightblue') {
-            return element.style.backgroundColor = "lightcoral";
+            return (document.getElementById(rowId).style.backgroundColor = "lightcoral");
         }
         if (element.style.backgroundColor === 'lightcoral') {
-            return element.style.backgroundColor = "rgb(142, 252, 142)";
+            return document.getElementById(rowId).style.backgroundColor = "rgb(252, 196, 92)";
         }
-        if (element.style.backgroundColor === 'rgb(142, 252, 142)') {
-            return element.style.backgroundColor = "white";
+        if (element.style.backgroundColor === 'rgb(252, 196, 92)') {
+            return document.getElementById(rowId).style.backgroundColor = "rgb(255, 243, 132)";
+        }
+        if (element.style.backgroundColor === 'rgb(255, 243, 132)') {
+            return document.getElementById(rowId).style.backgroundColor = "lightgreen";
+        }
+        if (element.style.backgroundColor === 'lightgreen') {
+            return document.getElementById(rowId).style.backgroundColor = "lightblue";
+        }
+        if (element.style.backgroundColor === 'lightblue') {
+            return document.getElementById(rowId).style.backgroundColor = "rgb(192, 103, 255)";
+        }
+        if (element.style.backgroundColor === 'rgb(192, 103, 255)') {
+            return document.getElementById(rowId).style.backgroundColor = "rgb(255, 173, 255)";
+        }
+        if (element.style.backgroundColor === 'rgb(255, 173, 255)') {
+            return document.getElementById(rowId).style.backgroundColor = "white";
         }
 
     }
 
     const submitUpdate = value => {
+
         let prevValue = updateItem;
         updateTodo(edit.id, value, prevValue)
         // console.log("AMAMAMAM:", prevValue);
@@ -95,15 +123,15 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
             <div ref={rowRef} key={index} className={todo.isComplete ? 'todo-row complete' : 'todo-row'} >
                 <Draggable key={todo.id} draggableId={todo.id} index={index} >
                     {(provided, snapshot) => (
-                        <div id={index} className="list-item" ref={provided.innerRef}
+                        <div id={todo.id} className="list-item" ref={provided.innerRef}
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
-                            style={getItemStyle(
-                                snapshot.isDragging,
-                                provided.draggableProps.style,
-                                index
-                                // currentClr
-                            )}>
+                            style={
+                                getItemStyle(
+                                    snapshot.isDragging,
+                                    provided.draggableProps.style,
+                                    todo.id
+                                )}>
 
                             <div className="todo-text" key={todo.id} onClick={() => completeTodo(todo.id, completeTxt = filterText(todo.text))}>
                                 {todo.text}
@@ -116,13 +144,14 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
                                 </div>
                                 <div className="edit-icon">
                                     <FaRegEdit size={28} onClick={() => {
+                                        setTrigger(false);
                                         setEdit({ id: todo.id, value: filterText(todo.text) });
                                         updateItem = filterText(todo.text);
 
                                     }} />
                                 </div>
                                 <div className="change-color-icon">
-                                    <HiOutlineColorSwatch size={28} onClick={() => { changeColor(index, provided) }} />
+                                    <HiOutlineColorSwatch size={28} onClick={() => { changeColor(todo.id, provided) }} />
                                 </div>
                             </div>
                         </div>
